c7df31816b1a492a672e9185ff0642f79e268679
윤연경 <amyss0907@gmail.com>
2021-03-18 13:50:28 +0900

    project settings

---
+++ farming_springboot/src/main/java/tp/farming_springboot/FarmingSpringbootApplication.java
@@ -0,0 +1,14 @@
+package tp.farming_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmingSpringbootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FarmingSpringbootApplication.class, args);
+    }
+
+}
---
+++ farming_springboot/src/test/java/tp/farming_springboot/FarmingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.farming_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmingSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
