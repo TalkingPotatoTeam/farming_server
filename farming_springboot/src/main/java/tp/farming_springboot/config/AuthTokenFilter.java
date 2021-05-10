80137a8e26fc0b4c59cb1b03d07640588623e879
윤연경 <amyss0907@gmail.com>
2021-05-10 14:26:09 +0900

    refactor: delete unnecessary imports

--- farming_springboot/src/main/java/tp/farming_springboot/config/AuthTokenFilter.java
+++ farming_springboot/src/main/java/tp/farming_springboot/config/AuthTokenFilter.java
@@ -20,7 +20,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import tp.farming_springboot.domain.user.repository.UserRepository;
