96b494efbef4747a93996cac73b9f48efcb36341
윤연경 <amyss0907@gmail.com>
2021-06-05 19:53:06 +0900

    feat: chat functions

--- farming_springboot/src/main/java/tp/farming_springboot/controller/ChatController.java
+++ farming_springboot/src/main/java/tp/farming_springboot/controller/ChatController.java
@@ -5,10 +5,13 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import tp.farming_springboot.domain.chat.model.ChatMessage;
 import tp.farming_springboot.domain.chat.model.ChatNotification;
 import tp.farming_springboot.domain.chat.service.ChatMessageService;
 import tp.farming_springboot.domain.chat.service.ChatRoomService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ChatController {
@@ -32,5 +35,26 @@ public class ChatController {
                         saved.getId(),
                         saved.getSenderId(),
                         saved.getSenderName()));
+    }
+    @GetMapping("/messages/{senderId}/{recipientId}/count")
+    public ResponseEntity<Long> countNewMessages(
+            @PathVariable String senderId,
+            @PathVariable String recipientId) {
+
+        return ResponseEntity
+                .ok(chatMessageService.countNewMessages(senderId, recipientId));
+    }
+
+    @GetMapping("/messages/{senderId}/{recipientId}")
+    public ResponseEntity<?> findChatMessages ( @PathVariable String senderId,
+                                                @PathVariable String recipientId) {
+        return ResponseEntity
+                .ok(chatMessageService.findChatMessages(senderId, recipientId));
+    }
+
+    @GetMapping("/messages/{id}")
+    public ResponseEntity<?> findMessage ( @PathVariable String id) {
+        return ResponseEntity
+                .ok(chatMessageService.findById(id));
     }
 }
